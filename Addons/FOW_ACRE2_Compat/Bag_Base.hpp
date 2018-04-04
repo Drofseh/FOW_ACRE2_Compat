63d8a6cf366c3d9d890a32e4dc1b747ffb712790
Drofseh <drofseh@gmail.com>
2018-04-04 01:02:25 -0700

    Fix TransportItems in backpack, update version num

--- Addons/FOW_ACRE2_Compat/Bag_Base.hpp
+++ Addons/FOW_ACRE2_Compat/Bag_Base.hpp
@@ -3,25 +3,34 @@ class Bag_Base;
 
     class fow_b_torn_radio : B_LIB_AssaultPack_Base {
         maximumLoad = 120;
-        // None of the Transport classes appear to be able to add ACRE radios
-        /*
-        class TransportWeapons {
+        class TransportItems {
             class _xx_ACRE_PRC77 {
                 count = 1;
-                weapon = "ACRE_PRC77";
+                name = "ACRE_PRC77";
             };
         };
-        */
     };
 
     class fow_b_us_radio : Bag_Base {
         maximumLoad = 120;
+        class TransportItems {
+            class _xx_ACRE_PRC77 {
+                count = 1;
+                name = "ACRE_PRC77";
+            };
+        };
     };
 
 class fow_b_uk_p37;
 
     class fow_b_uk_p37_radio : fow_b_uk_p37 {
         maximumLoad = 200;
+        class TransportItems {
+            class _xx_ACRE_PRC77 {
+                count = 1;
+                name = "ACRE_PRC77";
+            };
+        };
     };
 
     class fow_b_uk_p37_radio_blanco : fow_b_uk_p37_radio {
